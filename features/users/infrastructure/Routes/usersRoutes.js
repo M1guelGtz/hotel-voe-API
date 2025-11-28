8a170762370af626b4c83c65273fa990e0e8f60a
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2025-11-28 11:59:31 -0600

    Added: user routes

--- features/users/infrastructure/Routes/usersRoutes.js
+++ features/users/infrastructure/Routes/usersRoutes.js
@@ -3,11 +3,15 @@ const express = require('express');
 module.exports = function productRoutes(controller) {
     const router = express.Router();
 
-    // POST /users -> create
     router.post('/', (req, res) => controller.createUser(req, res));
+    router.get('/', (req, res) => controller.getUsers(req, res));
+    router.put('/:id', (req, res) => controller.putUsers(req, res));
+    router.delete('/:id', (req, res) => controller.deleteUsers(req, res));
+    router.get('/:id', (req, res) => controller.getUserById(req, res));
+    router.get('/email/:email', (req, res) => controller.getUserByEmail(req, res));
+    router.post('/login', (req, res) => controller.loginUser(req, res));
+    router.post('/register', (req, res) => controller.registerUser(req, res));
 
-    // GET /users -> list
-    router.get('/user', (req, res) => controller.getUsers(req, res));
-
+    
     return router;
 };
