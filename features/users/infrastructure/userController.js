10f887e82811bea95e3cd4424251262cf988e8e5
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2025-11-28 11:56:59 -0600

    Added: functions in userController

--- features/users/infrastructure/userController.js
+++ features/users/infrastructure/userController.js
@@ -13,6 +13,28 @@ class UserController {
     getUsers(req, res) {
         return this.getController.handle(req, res);
     }
+    putUsers(req, res) {
+        return this.putController.handle(req, res);
+    }
+
+    deleteUsers(req, res) {
+        return this.deleteController.handle(req, res);
+    }
+
+    getUserById(req, res) {
+        return this.getByIdController.handle(req, res);
+    }
+    getUserByEmail(req, res) {
+        return this.getByEmailController.handle(req, res);
+    }
+
+    loginUser(req, res) {
+        return this.loginController.handle(req, res);
+    }
+
+    registerUser(req, res) {
+        return this.registerController.handle(req, res);
+    }
 }
 
 module.exports = UserController;
